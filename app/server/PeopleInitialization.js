8eaa52c6b75d8440d7213a39bb2dcfdd6ea5d441
In Woo Park <48573161+inwooCS@users.noreply.github.com>
2020-03-07 19:50:55 -1000

    Task 6 Finished

--- app/server/PeopleInitialization.js
+++ app/server/PeopleInitialization.js
@@ -1,8 +1,12 @@
 import { people } from '../both/PeopleCollection.js';
 
-people.insert({ first: 'Philip', last: 'Johnson', age: 60, city: 'Kailua'});
-people.insert({ first: 'Joanne', last: 'Amberg', age: 58, city: 'Kailua' });
-people.insert({ first: 'Jenna', last: 'Corin', age: 31, city: 'Boulder Creek' });
-people.insert({ first: 'Katie', last: 'Kai', age: 25, city: 'Palo Alto' });
-
-people.insert({ first: 'Doris', last: 'Johnson', age: 93, city: 'Naples' });
+/*
+ *  specify that these “initial” inserts should be done only when the collection is empty
+ */
+if (people.find().count() === 0) {
+  people.insert({ first: 'Philip', last: 'Johnson', age: 60, city: 'Kailua' });
+  people.insert({ first: 'Joanne', last: 'Amberg', age: 58, city: 'Kailua' });
+  people.insert({ first: 'Jenna', last: 'Corin', age: 31, city: 'Boulder Creek' });
+  people.insert({ first: 'Katie', last: 'Kai', age: 25, city: 'Palo Alto' });
+  people.insert({ first: 'Doris', last: 'Johnson', age: 93, city: 'Naples' });
+}
